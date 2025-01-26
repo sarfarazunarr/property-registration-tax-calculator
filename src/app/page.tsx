dd427875d14f7ebdd5643ee7498ba6cc427cbbe8
Sarfaraz Unar <130893009+sarfarazunarr@users.noreply.github.com>
2025-01-26 16:36:13 +0500

    notification for update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,11 @@ const Home = () => {
   return (
     <div className='main_container'>
       <h1>Land Registry Taxes Calculation</h1>
-      <p>Currently it shows data of Hala, Saeedabad, Matiari</p>
+      <p>This project is now under <a href="https://techryzer.com">Techryzer</a>.</p>
+      <p className={"mb-20"}>Currently it shows data of Hala, Saeedabad, Matiari</p>
       <LandMainCompnent />
+      <p className="notification_message">Soon we are enabling users to add thier own valuation data!</p>
+      <p className="notification_message">Some new features will be added soon!</p>
     </div>
   )
 }
